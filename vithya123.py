b6d3c009e4a6ff455a822399e4a5ab4819620ffe
vithyasri <52270841+vithyasri@users.noreply.github.com>
2019-06-30 22:06:22 +0530

    Create vithya123.py

---
+++ vithya123.py
@@ -0,0 +1,10 @@
+def getPairsCount(arr, n, sum): 
+ 
+    count = 0
+ 
+    for i in range(0, n): 
+        for j in range(i + 1, n): 
+            if arr[i] + arr[j] == sum: 
+                count += 1
+ 
+    return count 
